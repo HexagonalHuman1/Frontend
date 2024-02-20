7fdf98b36ccf6586f3e4de69c2db9e1072482725
Jun279 <33.beautifulboy@gmail.com>
2024-02-21 01:02:55 +0900

    할 일 목록 페이지 ui 수정

--- src/todo/SelectedTodoList.jsx
+++ src/todo/SelectedTodoList.jsx
@@ -1,10 +1,11 @@
 import styled from "styled-components";
 
 const ListTextContainer = styled.div`
+  width: 19rem;
   display: flex;
-  gap: 9.38rem;
   margin-left: 1.56rem;
   margin-top: 1.12rem;
+  position: relative;
 `
 
 const RecommendList = styled.p`
@@ -17,7 +18,7 @@ const RecommendList = styled.p`
   margin: 0;
 `
 
-const AddList = styled.p`
+const DeleteList = styled.p`
   color: #DD8EA4;
   font-family: Inter;
   font-size: 0.75rem;
@@ -26,13 +27,17 @@ const AddList = styled.p`
   line-height: normal;
   cursor: pointer;
   margin: 0;
+  position: absolute;
+  right: 0;
 `
 
 export default function TodoList({content}) {
   return (
     <ListTextContainer>
-      <RecommendList>{content}</RecommendList>
-      <AddList>삭제하기</AddList>
+      <div>
+        <RecommendList>{content}</RecommendList>
+      </div>
+      <DeleteList>삭제하기</DeleteList>
     </ListTextContainer>
   )
 }
--- src/todo/TodoList.jsx
+++ src/todo/TodoList.jsx
@@ -1,10 +1,11 @@
 import styled from "styled-components";
 
 const ListTextContainer = styled.div`
+  width: 19rem;
   display: flex;
-  gap: 9.38rem;
   margin-left: 1.56rem;
   margin-top: 1.12rem;
+  position: relative;
 `
 
 const RecommendList = styled.p`
@@ -26,12 +27,16 @@ const AddList = styled.p`
   line-height: normal;
   cursor: pointer;
   margin: 0;
+  position: absolute;
+  right: 0;
 `
 
 export default function TodoList({content}) {
   return (
     <ListTextContainer>
-      <RecommendList>{content}</RecommendList>
+      <div>
+        <RecommendList>{content}</RecommendList>
+      </div>
       <AddList>추가하기</AddList>
     </ListTextContainer>
   )
