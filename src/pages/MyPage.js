c5f725e5ba94975eb587fce0c96836717c9f76ac
minseo121 <kittse1217@gmail.com>
2024-02-22 02:34:25 +0900

    fix : mypage

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -3,6 +3,16 @@ import '../css/MyPage.css';
 import { Link } from 'react-router-dom';
 import styled from "styled-components";
 import Nav from '../components/Nav';
+import human from '../img/human.png';
+
+const ImgContainer = styled.div`
+    margin-right:15px;
+`;
+
+const Img = styled.img`
+    width: 90px;
+    height: 90px;
+`;
 
 const StyledLink = styled(Link)`
     text-decoration: none; 
@@ -14,8 +24,9 @@ const MyPage = () => {
     return (
         <div className='mypage_container'>
             <div className='profile'>
-                <div className='profile_img'>
-                </div>
+                <ImgContainer>
+                    <Img src={human} />
+                </ImgContainer>
                 <div className='profile_content'>
                     <div>
                         <p><span className='profile_name'>사용자 이름</span><br/>
